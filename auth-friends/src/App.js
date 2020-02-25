46fdeb5dbec1d41b4328c743c5f2d00bc9470e81
Adebola  Adesina <dreapap@gmail.com>
2020-02-25 14:53:40 -0800

    initial Form setup

---
+++ auth-friends/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import LoginForm from "./LoginForm/LoginForm";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Friend's Login</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
