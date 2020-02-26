fe4c4d0c215097aa5965594942b9517befc1e6b9
Adebola  Adesina <dreapap@gmail.com>
2020-02-26 09:13:34 -0800

    update

--- auth-friends/src/components/privateRoute.js
+++ auth-friends/src/components/privateRoute.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 const privateRoute = ({ component: Component, ...rest }) => {
-  //const Component = component;
   const token = window.localStorage.getItem("token");
   return (
     <Route
       {...rest}
       render={() => {
         if (token) {
-          //return the component
           return <Component {...rest} />;
         } else {
-          //redirect the user to login
           return <Redirect to='/login' />;
         }
       }}
